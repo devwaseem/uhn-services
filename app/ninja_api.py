38d26c648ee5296268d2e6326a877e8e413589fc
Waseem Akram <waseem07799@gmail.com>
2026-03-14 01:04:28 +0530

    chore: Update Ninja API title, description and version

--- app/ninja_api.py
+++ app/ninja_api.py
@@ -5,6 +5,10 @@ from app.solutions.contract_workspaces.api import (
 )
 from app.solutions.non_po_invoices.api import router as non_po_invoices_router
 
-api = NinjaAPI()
+api = NinjaAPI(
+    title="UHN Services - API",
+    version="0.1.0",
+    description="Enhancements developed for UHN by Charing Cross Capital",
+)
 api.add_router("/non-po-invoices/", non_po_invoices_router)
 api.add_router("/contract-workspaces/", contract_workspaces_router)
